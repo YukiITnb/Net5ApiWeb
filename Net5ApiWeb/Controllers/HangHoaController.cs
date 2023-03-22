4276432c933d6a6f80fd45b88c27b4e79c1405e3
yukiITnb <yuki1132001@gmail.com>
2023-03-23 00:06:56 +0700

    Add project files.

---
+++ Net5ApiWeb/Controllers/HangHoaController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Net5ApiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net5ApiWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hanghoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hanghoas);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            try
+            {
+                // Query
+                var hanghoa = hanghoas.SingleOrDefault(hh => hh.HangHoaGuid == Guid.Parse(id));
+                if (hanghoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hanghoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(HangHoaVM hanghoaVM)
+        {
+            var hanghoa = new HangHoa
+            {
+                HangHoaGuid = Guid.NewGuid(),
+                TenHangHoa = hanghoaVM.TenHangHoa,
+                Gia = hanghoaVM.Gia,
+            };
+            hanghoas.Add(hanghoa);
+            return Ok(new
+            {
+                Success = true, Data = hanghoa
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(string id,HangHoa hanghoaput)
+        {
+            try
+            {
+                // Query
+                var hanghoa = hanghoas.SingleOrDefault(hh => hh.HangHoaGuid == Guid.Parse(id));
+                if (hanghoa == null)
+                {
+                    return NotFound();
+                }
+                if(id != hanghoaput.HangHoaGuid.ToString())
+                {
+                    return BadRequest();
+                }
+                // Update
+                hanghoa.TenHangHoa = hanghoaput.TenHangHoa;
+                hanghoa.Gia = hanghoaput.Gia;
+                return Ok(hanghoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            try
+            {
+                // Query
+                var hanghoa = hanghoas.SingleOrDefault(hh => hh.HangHoaGuid == Guid.Parse(id));
+                if (hanghoa == null)
+                {
+                    return NotFound();
+                }
+                hanghoas.Remove(hanghoa);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Net5ApiWeb/Models/HangHoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Net5ApiWeb.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double Gia { get; set; }
+    }
+
+    public class HangHoa : HangHoaVM
+    {
+        public Guid HangHoaGuid { get; set;}
+    }
+}
